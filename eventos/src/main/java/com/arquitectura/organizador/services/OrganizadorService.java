ed21c18dba335b22b8ae2fc13a6e518779f6562a
Azgrel <ivelasquezd1@unicartagena.edu.co>
2025-08-11 20:04:20 -0500

    Eliminacion de microservicio de eventos

--- eventos/src/main/java/com/arquitectura/organizador/services/OrganizadorService.java
+++
@@ -1,7 +0,0 @@
-package com.arquitectura.organizador.services;
-
-import com.arquitectura.organizador.entity.Organizador;
-import com.arquitectura.services.CommonServiceString;
-
-public interface OrganizadorService extends CommonServiceString<Organizador> {
-}
--- eventos/src/main/java/com/arquitectura/tipo/service/TipoService.java
+++
@@ -1,7 +0,0 @@
-package com.arquitectura.tipo.service;
-
-import com.arquitectura.services.CommonService;
-import com.arquitectura.tipo.entity.Tipo;
-
-public interface TipoService extends CommonService<Tipo> {
-}
