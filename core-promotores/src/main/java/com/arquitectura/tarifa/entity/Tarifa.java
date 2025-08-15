120cc2fab4d4533baf8fb06436633a524b9a6fa0
Azgrel <ivelasquezd1@unicartagena.edu.co>
2025-08-14 19:17:55 -0500

    json Ignore properties

--- core-promotores/src/main/java/com/arquitectura/tarifa/entity/Tarifa.java
+++ core-promotores/src/main/java/com/arquitectura/tarifa/entity/Tarifa.java
@@ -4,6 +4,7 @@ import com.arquitectura.localidad.entity.Localidad;
 import com.arquitectura.ticket.entity.Ticket;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tarifas")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Tarifa {
 
     @Id
