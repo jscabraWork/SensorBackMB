972efcc68d68faa13d8a2a2983cc4708ef2a89b5
Azgrel <ivelasquezd1@unicartagena.edu.co>
2025-08-14 19:03:29 -0500

    Validacion al rechazar orden

--- core-pagos/src/main/java/com/arquitectura/orden/entity/Orden.java
+++ core-pagos/src/main/java/com/arquitectura/orden/entity/Orden.java
@@ -131,7 +131,13 @@ public class Orden extends Auditable {
 
     public void rechazar(){
         estado = 2; // Rechazada
-        liberarTickets();
+
+        //SOLO LIBEARAR TICKETS DE LA ORDEN SI ES DE TIPO 1 (COMPRA ESTANDAR) O 3 (CREAR ALCANCIA)
+        //No se pueden liberar tickets de ordenes de tipo 4 (aportaciones a alcancía), 5 (traspaso) o 6 (asignación)
+        if(tipo == 1 || tipo == 3) {
+            liberarTickets();
+        }
+
         if(transacciones!= null && !transacciones.isEmpty()) {
             transacciones.forEach(transaccion -> rechazar());
         }
